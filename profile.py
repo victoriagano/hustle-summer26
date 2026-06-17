917bfb771e4f0c39d9311336020da9e1b9e7a9d4
Victoria <victoriagano9@gmail.com>
2026-06-17 16:48:06 -0700

    Add Python profile variables lab

---
+++ profile.py
@@ -0,0 +1,18 @@
+full_name = "Victoria Gano"
+
+nickname = "Tori"
+
+age = 17
+
+used_python_before = False
+
+hobbies = ["watching movies", "listening to music","baking"]
+
+favorites = {"movie":"A Walk to Remember", "food":"pizza","hobbie":"baking"}
+
+print(full_name, nickname, age, used_python_before, hobbies, favorites)
+
+skills = ("teamwork","communication","leadership")
+languages_spoken = {"English"}
+
+print(full_name, nickname, age, used_python_before, hobbies, favorites, skills, languages_spoken)
